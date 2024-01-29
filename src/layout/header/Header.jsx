90d0fafb783863bed70c53498c00400ada7f99a2
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2024-01-29 22:08:34 +0500

    Header section done

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
+import LogoImg from "../../assets/img/Logo_header.png";
+import SearchIcon from "../../assets/icon/SearchIcon"
+import NotificationIcon from "../../assets/icon/NotificationIcon"
 
 function Header(props) {
-    return (
-        <div className=''>
-            header
-        </div>
-    );
+  return (
+    <div className=" container  flex justify-between items-center py-6 px-6 bg-transparent">
+      <img src={LogoImg} alt="logo" />
+      <nav>
+        <ul className="flex items-center justify-between rounded-lg bg-black py-2 px-9 gap-8">
+          <li className="  py-2 px-4 text-white rounded-lg hover:bg-[#1A1A1A]">
+            <a href="#"> Home</a>
+          </li>
+          <li className="  py-2 px-4 text-white rounded-lg hover:bg-[#1A1A1A]">
+            <a href="#"> Movies & Shows</a>
+          </li>
+          <li className=" py-2 px-4 text-white rounded-lg hover:bg-[#1A1A1A]">
+            <a href="#">Support</a>
+          </li>
+          <li className="  py-2 px-4 text-white rounded-lg hover:bg-[#1A1A1A]">
+            <a href="#"> Subscriptions</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="flex items-center justify-center  gap-7 ">
+        <span className="cursor-pointer"><SearchIcon/></span>
+        <span className="cursor-pointer"><NotificationIcon/></span>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
